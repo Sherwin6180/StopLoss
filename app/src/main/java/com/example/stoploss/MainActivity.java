56adc2abd900f944b297a0e272a076438b556e05
Sherwin6180 <sherwin6180@gmail.com>
2022-01-03 20:21:11 -0600

    finish splash screen

---
+++ app/src/main/java/com/example/stoploss/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.stoploss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/stoploss/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.stoploss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Animation imageAnimation = AnimationUtils.loadAnimation(this, R.anim.image_animation);
+
+        imageView.setAnimation(imageAnimation);
+
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+}
